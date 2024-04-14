42d1e785c841b53c9d8b2a3b92608a7d5e9238ce
Mohammad <mohammadweex21@gmail.com>
2024-04-15 00:51:54 +0500

    connect to db

--- api/index.js
+++ api/index.js
@@ -1,4 +1,17 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+// Connect to MongoDB
+mongoose.connect(process.env.MONGO).then(() => {
+    console.log('Connected to MongoDB');
+    }
+);
+
+
+// Express
 
 const app = express();
 
